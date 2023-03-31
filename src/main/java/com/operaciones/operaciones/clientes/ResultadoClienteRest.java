ae68b2c5f530ada41e8550abed43e8c00c035b8c
VargasSantiago <vargas.santiago2002@gmail.com>
2023-03-31 16:29:29 -0300

    first commit

---
+++ src/main/java/com/operaciones/operaciones/clientes/ResultadoClienteRest.java
@@ -0,0 +1,13 @@
+package com.operaciones.operaciones.clientes;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "resultados", url = "localhost:9001")
+public interface ResultadoClienteRest {
+
+    @GetMapping("/resultado/{numero}")
+    public String mostrarResultados(@PathVariable Float numero);
+
+}
---
+++ src/main/java/com/operaciones/operaciones/controllers/OperacionController.java
@@ -0,0 +1,29 @@
+package com.operaciones.operaciones.controllers;
+
+import com.operaciones.operaciones.clientes.ResultadoClienteRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OperacionController {
+
+    @Autowired
+    private RestTemplate clienteRest;
+
+    @Autowired
+    private ResultadoClienteRest clienteFeing;
+
+    @GetMapping("/sumar")
+    public String sumar(@RequestParam float a, @RequestParam float b) {
+        String resultado = Float.toString(a + b);
+        return clienteRest.getForObject("http://localhost:9001/resultado/" + resultado, String.class);
+    }
+
+    @GetMapping("/restar")
+    public String restar(@RequestParam float a, @RequestParam float b) {
+        return clienteFeing.mostrarResultados(a - b);
+    }
+}
